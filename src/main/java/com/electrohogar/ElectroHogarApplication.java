56e0e4a76dc23230021284918c64e6ca09d5b144
develasmen50931 <develasmen@gmail.com>
2024-02-01 15:47:48 -0600

    Commit de EA-08-Practicas-2 para semana 4 //// Desarrollo de Aplicaciones Web y Patrones Campus Virtual 2024 Primer Cuatrimestre

---
+++ src/main/java/com/electrohogar/ElectroHogarApplication.java
@@ -0,0 +1,13 @@
+package com.electrohogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroHogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroHogarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electrohogar/ElectroHogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrohogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroHogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
